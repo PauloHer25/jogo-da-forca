51e191623ded61de72f711b327e5bf358deda1e3
root <paulo.her2100@gmail.com>
2022-07-23 18:20:24 -0300

    Lógica do jogo da forca

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,16 +1,55 @@
 class Forca {
+  constructor(palavra) {
+      this.palavra = palavra;
+      this.vidas = 6;
+      this.letrasChutadas = [];
+  }
+  chutar(letra) {
+      if (this.letrasChutadas.includes(letra)) {
+          return null;
+      }
+      if (letra.length != 1) {
+          return null;
+      }
 
-  chutar(letra) { }
+      if (!(this.palavra.includes(letra))) {
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+          this.vidas -= 1;
+      }
+      this.letrasChutadas.push(letra);
+      return letra;
+  }
+  buscarEstado() {
+      if (this.vidas == 0) {
+          return "perdeu";
+      }
+      for (let index = 0; index < this.palavra.length; index++) {
+          const letra = this.palavra[index];
 
+          if (!(this.letrasChutadas.includes(letra))) {
+              return "aguardando chute";
+          }
+      }
+      return "ganhou";
+  }
+  // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
   buscarDadosDoJogo() {
+      let palavra = [];
+
+      for (let index = 0; index < this.palavra.length; index++) {
+          const letra = this.palavra[index];
+
+          if (this.letrasChutadas.includes(letra)) {
+              palavra.push(letra);
+          } else {
+              palavra.push("_");
+          }
+      }
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+          vidas: this.vidas, // Quantidade de vidas restantes
+          palavra: palavra, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas 
       }
   }
 }
-
 module.exports = Forca;
